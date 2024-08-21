9d1b016d486983443cc0ef377dc52b1000dd0010
Bahattin Celik <celik@wave-solutions.de>
2024-08-21 10:16:33 +0200

    first

---
+++ Expresso/Expresso/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Expresso.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Expresso
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new NavigationPage(new MainPage());
+            MainPage = new HomePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Expresso/Expresso/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Expresso
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void BtnNavigation_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Bestellung());
+        }
+    }
+}
